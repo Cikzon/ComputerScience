8221c2f44c0a0561a0bcfb71696c81181f124ba4
cikzon <826095@mystma.org>
2024-09-30 15:01:12 -0500

    9/30/24

---
+++ Assignments/fav movies.py
@@ -0,0 +1,8 @@
+print("mathias' fav games")
+print("hi-fi rush")
+print("assasins creed 3 ")
+print("borderlands 2")
+print("call of duty world at war")
+print("dead rising")
+print("subnautica")
+print("crackdown")
---
+++ Assignments/funtions.py
@@ -0,0 +1,17 @@
+def Favgames(game1, game2, game3, game4, game5, game6, game7):
+    print("Mias favorite gamez")
+    print("1.")
+    print()
+    print()
+    print()
+    print()
+    print()
+
+def add(X, Y):
+ print(X +Y)
+
+
+        
+add(5, 10)
+add(15, 15)
+add("tim", "Mia")
