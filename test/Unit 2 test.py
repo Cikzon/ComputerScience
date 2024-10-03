23aa475c4b4496dcfae4c0a84315ae3fd08156b6
cikzon <826095@mystma.org>
2024-10-03 15:00:36 -0500

    10/3/2024

---
+++ test/Unit 2 test.py
@@ -0,0 +1,6 @@
+print("give me 3 random words")
+one= input ("first word")
+two= input("second word")
+three= input("third word")
+print(f"you chose {one} as your first word then {two} for your second word, then finally chose {three} as your final word great job! ") 
+
